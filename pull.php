b498df80ca62724fbd410cebeaf63ff5ab48f055
Daniela-Colaborador <105184552+DanielaPortillo@users.noreply.github.com>
2024-02-15 13:54:13 -0600

    Commiteando para hacer PR
    Agregamos 4 lineas de codigo de php

--- pull.php
+++ pull.php
@@ -2,4 +2,11 @@
 
 echo "Añadiendo codigo PHP";
 
+echo "Añadiendo codigo para realizar el PR";
+echo "Añadiendo codigo para realizar el PR";
+
+echo "Añadiendo codigo para realizar el PR";
+echo "Añadiendo codigo para realizar el PR";
+
+
 ?>
